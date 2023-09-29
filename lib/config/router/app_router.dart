e118a66e9c4f2904089d1bd61bab4851295350bb
Rommel Olachea <msc.rommel.olachea@gmail.com>
2023-09-28 22:30:02 -0700

    inicializacion del proyecto

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+//Nota: utilizar GoRoute nos permite no utilizar configuraciones adicionales
+//si vamos a utilizar nuestro codigo en la web
+
+final appRouter = GoRoute(
+  path: '/',
+  routes: [
+    GoRoute(
+        path: '/',
+        name: HomeScreen.name,
+        builder: (context, state) => const HomeScreen(),
+      ),
+  ]
+);
