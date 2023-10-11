87663604c9afae114ce627553f26ca74d3cd1736
Rommel Olachea <msc.rommel.olachea@gmail.com>
2023-10-11 14:30:13 -0700

    Carrusel de imagenes

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -19,19 +20,51 @@ class MoviesSlideshow extends StatelessWidget {
             0.9, //la imagen anterior y posterior en el carrusel se ven mas peques
         autoplay: true, //se moveran automaticamente
         itemCount: movies.length,
-        itemBuilder: (context, index) => _slide(movie: movies[index]),
+        itemBuilder: (context, index) => _Slide(movie: movies[index]),
       ),
     );
   }
 }
 
-class _slide extends StatelessWidget {
+class _Slide extends StatelessWidget {
   final Movie movie;
-  const _slide({super.key, required this.movie});
+  const _Slide({super.key, required this.movie});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+            color: Colors.black45, //colod difuminado
+            blurRadius: 10, //crea un difuminado
+            offset: Offset(0, 10) // mueve la sombra
+            )
+      ],
+    );
+
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          //me permite ponerle los bordes redondeados a la imagen
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit
+                .cover, //esto hace que la imagen tome el espacio que le estamos dando
+            loadingBuilder: (context, child, loadingProgress) {
+              //me permite determinar si la imagen todavia esta proceso de loading o carga
+              if (loadingProgress != null) {
+                return const DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.black12));
+              }
+              return FadeIn(child: child);
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
-
