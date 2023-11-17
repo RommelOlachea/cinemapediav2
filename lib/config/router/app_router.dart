a9dc3c92cb585489075c0c2df11afa70b313de5c
Rommel Olachea <msc.rommel.olachea@gmail.com>
2023-11-16 17:11:12 -0700

    se obtuvo la pelicula y la lista de actores de la pelicula

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,13 +4,18 @@ import 'package:go_router/go_router.dart';
 //Nota: utilizar GoRoute nos permite no utilizar configuraciones adicionales
 //si vamos a utilizar nuestro codigo en la web
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-        path: '/',
-        name: HomeScreen.name,
-        builder: (context, state) => const HomeScreen(),
-      ),
-  ]
-);
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    name: HomeScreen.name,
+    builder: (context, state) => const HomeScreen(),
+  ),
+  GoRoute(
+    path: '/movie/:id',
+    name: MovieScreen.name,
+    builder: (context, state) {
+      final movieId = state.pathParameters['id'] ?? 'no-id';
+      return MovieScreen(movieId: movieId);
+    },
+  ),
+]);
--- lib/presentation/widgets/movies/movies_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieHorizontalListview extends StatefulWidget {
   final List<Movie> movies;
@@ -113,9 +114,12 @@ class _Slide extends StatelessWidget {
                     );
                   }
 
-                  return FadeIn(
-                      child:
-                          child); //FadeIn del paque te de animate do de fernando
+                  return GestureDetector(
+                    onTap: () => context.push('/movie/${movie.id}'), //navegamos con el path de navegacion, y pasamos el parametro
+                    child: FadeIn(
+                        child:
+                            child),
+                  ); //FadeIn del paque te de animate do de fernando
                 },
               ),
             ),
