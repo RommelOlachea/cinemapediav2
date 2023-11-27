29c89b7097641837deaa587172907c30ed9baa48
Rommel Olachea <msc.rommel.olachea@gmail.com>
2023-11-27 14:55:06 -0700

    Go router oficial implementado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,21 +1,87 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/home_views/home_view.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../presentation/views/views.dart';
 
 //Nota: utilizar GoRoute nos permite no utilizar configuraciones adicionales
 //si vamos a utilizar nuestro codigo en la web
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-    path: '/',
-    name: HomeScreen.name,
-    builder: (context, state) => const HomeScreen(),
-  ),
-  GoRoute(
-    path: '/movie/:id',
-    name: MovieScreen.name,
-    builder: (context, state) {
-      final movieId = state.pathParameters['id'] ?? 'no-id';
-      return MovieScreen(movieId: movieId);
+//con ShellRoute podemos navegar sin destruir el bottom navigator del HomeScreen, ni la vista a la que apunta la opcion
+
+final appRouter = GoRouter(
+  initialLocation: '/', 
+  routes: [
+  ShellRoute(
+    builder: (context, state, child) {
+      return HomeScreen(childView: child);
     },
-  ),
+    routes: [
+      GoRoute(
+        path: '/',     //este seria el path inicial del shellrouter., y el widget que se manda es el HomeView, como child al homeScreen
+        builder: (context, state) {
+          return const HomeView();
+        },
+        routes: [
+          //ruta hija que se manda llamar dentro del homeview 
+          GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              },
+            ),
+
+        ],
+      ),
+
+      GoRoute(
+        path: '/favorites',     
+        builder: (context, state) {
+          return const FavoritesView();
+        },
+      ),
+
+
+
+    ],
+  )
 ]);
+
+
+//Rutas padre/hijo paso de parametros
+// final appRouter = GoRouter(initialLocation: '/', routes: [
+//   GoRoute(
+//     path: '/',
+//     name: HomeScreen.name,
+//     builder: (context, state) => const HomeScreen(childView: HomeView(),),
+//     routes: [
+//             GoRoute(
+//               path: 'movie/:id',
+//               name: MovieScreen.name,
+//               builder: (context, state) {
+//                 final movieId = state.pathParameters['id'] ?? 'no-id';
+//                 return MovieScreen(movieId: movieId);
+//               },
+//             ),
+
+//     ],
+//   );
+
+//Rutas con manejo tradicional, y paso de parametros a un screenm
+// final appRouter = GoRouter(initialLocation: '/', routes: [
+//   GoRoute(
+//     path: '/',
+//     name: HomeScreen.name,
+//     builder: (context, state) => const HomeScreen(childView: HomeView(),),
+//   ),
+//   GoRoute(
+//     path: '/movie/:id',
+//     name: MovieScreen.name,
+//     builder: (context, state) {
+//       final movieId = state.pathParameters['id'] ?? 'no-id';
+//       return MovieScreen(movieId: movieId);
+//     },
+//   ),
+// ]);
--- lib/presentation/widgets/shared/custom_bottom_navigationbar.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigationbar.dart
@@ -1,27 +1,53 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigatorBar extends StatelessWidget {
   const CustomBottomNavigatorBar({super.key});
 
+  int getCurrentIndex(BuildContext context) {
+    // final String location = GoRouterState.of(context).location;
+    final String location = '/favorites';
+
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+      default:
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      elevation: 0, //con elevation en 0, no se marca la linea divisoria del bottom navigation bar
-      items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_max),
-              label: 'Inicio'
-              ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.label_outline),
-              label: 'Categorias'
-              ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_outline),
-              label: 'Favoritos'
-              ),
-        ]
-      );
+        currentIndex: getCurrentIndex(context),
+        onTap: (value) => onItemTapped(context, value),
+        elevation:
+            0, //con elevation en 0, no se marca la linea divisoria del bottom navigation bar
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.label_outline), label: 'Categorias'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
+        ]);
   }
 }
